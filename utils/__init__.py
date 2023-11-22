c3771f85c2bcff996f324ff0412fc609555197c5
Semen Borinskikh <ph.borinskikh@mail.ru>
2023-11-22 11:16:10 +0300

    update __init__ file

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .data_processing import balance_the_dataset
-from .graphics import plot_corr_matrix, plot_feature_importance, plot_class_separation, plot_metrics_hist
+from .graphics import plot_feature_importance, plot_metrics_hist
 
